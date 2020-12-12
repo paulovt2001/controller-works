9cf59256ec37e6c700d74fbb8dadcb6b82e506b9
paulovt2001 <paulovt2001@gmail.com>
2020-12-12 16:14:20 -0300

    Hello Worldgit add .!

---
+++ src/main/java/br/com/controllerworks/controllerworks/ControllerWorksApplication.java
@@ -0,0 +1,13 @@
+package br.com.controllerworks.controllerworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/controllerworks/controllerworks/ControllerWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controllerworks.controllerworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
